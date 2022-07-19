999eb16ee82ee227db6724b99aaedd24bc9376bc
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-07-19 16:45:06 -0500

    create routes to serve frontend

--- server.js
+++ server.js
@@ -3,30 +3,44 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
+const allNotes = require('./db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
-const allNotes = require('./db/db');
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 function createNewNote(body, notesArray) {
     const newNote = body;
     notesArray.push(newNote);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
-        JSON.stringify({ allNotes: notesArray }, null, 2)
+        JSON.stringify(notesArray, null, 2)
     );
     return newNote;
 }
 
-app.get('/api/notes', (req, res) => {
-    res.json(allNotes);
-});
-
 app.post('/api/notes', (req, res) => {
     req.body.id = allNotes.length.toString();
-    //unique id needed, find package on npm 
-
+    //unique id needed, find package on npm?
+    
     const newNote = createNewNote(req.body, allNotes);
 
     res.json(newNote);
